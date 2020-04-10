7e48aca217567ed5ac06ef03344cff4c5ade30b6
Jean-Emmanuel LONGUEVILLE <jean.emmanuel.longueville@univ-poitiers.fr>
2020-04-10 16:38:19 +0200

    a jout d'un export csv

--- coulbournanalysis/inst/Test.R
+++ coulbournanalysis/inst/Test.R
@@ -2,4 +2,18 @@ devtools::load_all()
 get_subjects_responses("Datas/s01/1_2014_05_29__09_48_00.csv", session_number = "s01")
 read_session(pathdir = "Datas/s01/")
 read_session(pathdir = "Datas/190219 copy/")
-compile_experiment(pathdir = "./Datas/")
+donnees <- compile_experiment(pathdir = "./Datas/")
+names_state(donnees, "./Datas/names.csv", "current state")
+write.csv2(x = donnees, file = "Test.csv", na = "")
+
+#TODO ajouter l'appel à la fonction de modification des States à partir du fichier de trad
+#### Old code ####
+#setnames(dataframe,3,'CorrectPort')
+# dataframe = subset(dataframe, Protocol != 11 & ((CorrectPort > 1 & CorrectPort < 8) | (CorrectPort > 13 & CorrectPort < 19)))
+# dataframe = melt(dataframe, c(1,2,3),variable.name = 'ResponsePort', value.name = 'NumResponses')
+
+#### On regroupe les réponses en un dataframe ####
+# data = aggregate(NumResponses ~ Protocol + Subject + CorrectPort + ResponsePort + CorrectResponse, data = data, FUN = sum)
+# data = arrange(data,Protocol,Subject,CorrectResponse,CorrectPort,ResponsePort)
+#### On ajoute la date à partir du nom du fichier aux données ####
+# data$Date = as.Date(substr(file,1,6),'%m%d%y')
